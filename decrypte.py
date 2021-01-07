2010724449b7415bb28b2711eda3c6ed75d75919
repl.it user <replituser@example.com>
2021-01-07 01:32:04 +0000

    decrypt.py

---
+++ decrypte.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from Crypto.PublicKey import RSA 
+
+
+
+
+
+with open('private.PEM','r') as fk
+     private = RSA.importKey(fk.read())
+     fp.close()
+
+
+fichier = input("le fichier a crypter: ")
+
+
+with open(fichier, "rb") as fp:
+    d_encrypt = fp.read()
+    fp.close()
+
+x= private.decrypt(d_encrypt)
+
+
+with open(fichier + '.decrypt', "wb") as fd:
+    fd.write(x)
+fd.close()
